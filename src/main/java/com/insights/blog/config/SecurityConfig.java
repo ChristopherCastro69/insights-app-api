fbbc923d3b8c1bb0e99e8fd23370b10c9a971a59
Topitops69 <topitops123@gmail.com>
2024-06-06 16:26:40 +0800

    hello

--- src/main/java/com/insights/blog/config/SecurityConfig.java
+++ src/main/java/com/insights/blog/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize -> authorize.requestMatchers("/api/v1/auth/**").permitAll()
+                .authorizeHttpRequests(authorize -> authorize.requestMatchers("/**").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
